3cda986e616941653a96fa09b622181800a22d12
roy.van.der.kant <r.vanderkant@casema.nl>
2024-01-21 22:55:52 +0100

    End of section  13

--- client/src/app/router/RequireAuth.tsx
+++ client/src/app/router/RequireAuth.tsx
@@ -1,12 +1,24 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { useAppSelector } from "../store/configureStore";
+import { toast } from "react-toastify";
 
-export default function RequireAuth() 
+interface Props {
+    roles?: string[];
+}
+
+export default function RequireAuth({roles}: Props) 
 {
     const user = useAppSelector(state=> state.account.user);
     const location = useLocation();
+    
     if(!user) {
         return <Navigate to="/login" state={{from: location}}/>
     }
+
+    if(roles && !roles.some(r=> user.roles?.includes(r))) {
+        toast.error("Not authorised to acces this area");
+        return <Navigate to={"/Catalog"}></Navigate>
+    }
+
     return <Outlet/>
 }
